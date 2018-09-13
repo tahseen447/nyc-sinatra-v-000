90b9cd5db04a5595a9b4bdd213f8106c9305ff66
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-09-13 22:54:46 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -27,7 +27,6 @@ class LandmarksController < ApplicationController
 
   patch '/landmarks/:id' do
     @landmark = Landmark.find_by_id(params[:id])
-    binding.pry
     @landmark.name = params[:landmark][:name]
     @landmark.year_completed = params[:landmark][:year_completed]
     @landmark.save
