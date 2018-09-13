f5649bc8dfd4cf485c7ed9cda504bd6324ce1074
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-09-13 22:47:45 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -17,11 +17,22 @@ class LandmarksController < ApplicationController
 
   get '/landmarks/:id' do
     @landmark = Landmark.find_by_id(params[:id])
-    if @landmark
     erb :'landmarks/show'
   end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'landmarks/edit'
   end
 
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    binding.pry
+    @landmark.name = params[:landmark][:name]
+    @landmark.year_completed = params[:landmark][:year_completed]
+    @landmark.save
+    redirect to "landmarks/#{@landmark.id}"
+  end
 
 
 end
