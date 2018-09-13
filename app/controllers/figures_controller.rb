93ce93cf8421c8a47cecb6ce41769dbd8abe410f
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-09-13 22:04:03 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,7 @@
 class FiguresController < ApplicationController
   # add controller methods
+  get '/figures' do
+  @figures = Figure.all
+  erb :'figures/index'
+end
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,27 @@
 class LandmarksController < ApplicationController
   # add controller methods
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(:name=>params[:name], :name=>params[:year_completed])
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    if @landmark
+    erb :'landmarks/show'
+  end
+  end
+
+
+
 end
