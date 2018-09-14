d2cbb9c5ff8db5a126ae6c2292945377be5082fc
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-09-14 08:16:45 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -15,7 +15,7 @@ class FiguresController < ApplicationController
       @figure.titles << Title.create(:name => params[:name])
     end
     if !params[:landmark][:name].empty? || !params[:landmark][:year_completed].empty?
-      @figure.landmarks << Landmark.create(:name =>params[:landmark][:name], :year_completed =>params[:landmark][:year_completed])
+      @figure.landmarks << Landmark.create(params[:landmark])
     end
     @figure.save
     redirect "/figures/#{@figure.id}"
